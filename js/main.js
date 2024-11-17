be15d79343e13899f8c0a0290e18de1fe30c9f89
ahmed <ahmed.b.01.12.97@gmail.com>
2024-11-17 07:05:28 +0200

    Fix the issue

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-let quote = ["“So many books, so little time.”","“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”","“A room without books is like a body without a soul.”","“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”","“You know you're in love when you can't fall asleep because reality is finally better than your dreams.”","“You only live once, but if you do it right, once is enough.”","“Be the change that you wish to see in the world.”","“In three words I can sum up everything I've learned about life: it goes on.”",];
-
+let quote = [
+  "“So many books, so little time.”",
+  "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
+  "“A room without books is like a body without a soul.”",
+  "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”",
+  "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.”",
+  "“You only live once, but if you do it right, once is enough.”",
+  "“Be the change that you wish to see in the world.”",
+  "“In three words I can sum up everything I've learned about life: it goes on.”",
+];
 
 function displayQuote(quote) {
-    let quoteElement = document.getElementById("quoteContainer");
-    quoteElement.innerHTML = quote[Math.floor(Math.random()*quote.length)];
+  let random = Math.floor(Math.random() * quote.length);
+  while (random === Math.floor(Math.random() * quote.length)) {
+    random = Math.floor(Math.random() * quote.length);
+  }
+  let quoteElement = document.getElementById("quoteContainer");
+  quoteElement.innerHTML = quote[random];
 }
 
-
-function displayq(){
-    displayQuote(quote);
+function displayq() {
+  displayQuote(quote);
 }
